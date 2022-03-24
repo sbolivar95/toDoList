277e4993e3b0227f94fda8d0d2f693bf3748c103
Sebastian <sebastianbolivar95@gmail.com>
2022-03-24 00:31:32 -0400

    big update

--- script.js
+++ script.js
@@ -31,3 +31,23 @@ addButton.addEventListener('click', function () {
 
   createTask.innerHTML = textValue.value;
 });
+
+    //Leaflet library adding tile layers to map
+    L.tileLayer(
+      'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2JvbGl2YXI5NSIsImEiOiJja3poaW5hMTAxOXB3MzFvYmdpcWI0ODR6In0.oI4-d-N66_1DlhrX_Fqs0A',
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          'pk.eyJ1Ijoic2JvbGl2YXI5NSIsImEiOiJja3poaW5hMTAxOXB3MzFvYmdpcWI0ODR6In0.oI4-d-N66_1DlhrX_Fqs0A',
+      }
+    ).addTo(map);
+  },
+  function () {
+    alert('Could not get your position, browser needs your current position');
+  }
+);
